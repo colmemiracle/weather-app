0abea95fa59b0ae066872751035971b6e258ea1b
Gulmira <96774342+colmemiracle@users.noreply.github.com>
2022-02-16 00:52:05 +0600

    Delete src directory

--- src/draft.js
+++
@@ -1,6 +0,0 @@
-gettingWeather = async () =>{
-  const api_url = await
-  fetch("http://api.openweathermap.org/data/2.5/weather?q=${town.value}&appid=9ed2cc0d52d0095dc17fc83a4ee6b92e");
-  const data = await api_url.json();
-  console.log(data)
-}
